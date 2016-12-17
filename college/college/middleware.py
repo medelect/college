b7000280febf57cd09b22cc88aae8af709480033
alexGrut <medelect.axe@gmail.com>
2016-12-17 02:13:39 +0200

    2016-13-17_02:12 upd:Taglink;Local_kitty_pick

--- college/college/middleware.py
+++ college/college/middleware.py
@@ -4,10 +4,13 @@ from django.utils.deprecation import MiddlewareMixin
 
 class SQLMiddleware(MiddlewareMixin):
     def process_response ( self, request, response ):
+        h = '<p style="background-color: #9df; border: 2px solid #00f;'\
+                'border-radius: 5px;">' \
+                '<a href="/">Back to HOME</a></p>'
         t = sum([ float(i['time']) for i in connection.queries])
         c = len(connection.queries)
         stng= '<p style="color:#0b0">timeSum: %s, queryAmount: %s</p>'
-        response.content = '%s%s' % ( response.content, stng%(t,c))
+        response.content = '%s%s%s' % (h, response.content, stng%(t,c))
 
         return response
 
--- college/students/templatetags/ref_tag.py
+++ college/students/templatetags/ref_tag.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.inclusion_tag('./students/ref.html')
 def edit_tag(obj):
     class_name = str(obj.__doc__).split('(')[0].lower()
-    obj_link = '../admin/students/%s/%s/change/'%(class_name, obj.id)
+    obj_link = '../../admin/students/%s/%s/change/'%(class_name, obj.id)
     return {'obj':obj,'obj_link':obj_link}
 
 
