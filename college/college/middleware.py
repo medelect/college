b6aae11ca46dd2b89068003aed88d53c3110e22d
alexGrut <medelect.axe@gmail.com>
2016-12-19 01:35:18 +0200

    2016-35-19_01:12 UPD:middleware(terms)

--- college/college/middleware.py
+++ college/college/middleware.py
@@ -4,16 +4,29 @@ from django.utils.deprecation import MiddlewareMixin
 
 class SQLMiddleware(MiddlewareMixin):
     def process_response ( self, request, response ):
-        h = '<p style="background-color: #9df; border: 2px solid #00f;'\
-                'border-radius: 5px;">' \
-                '<a href="/">Back to HOME</a></p>'
-        ll = "'students/logout'"
-        l = '<br><button onclick="window.location.href=%s">Logout</button>'%(ll)
         t = sum([ float(i['time']) for i in connection.queries])
         c = len(connection.queries)
-        stng= '<p style="color:#0b0">timeSum: %s, queryAmount: %s</p>'
-        response.content = '%s%s%s%s' % (h, response.content, l,stng%(t,c))
+        stng= '<p style="color:#0b0">timeSum: %s, queryAmount: %s</p></body>'
 
+        if 'content="text/html;' in response.content:
+            response.content = response.content.replace('</body>', stng%(t,c))
         return response
+
+class HeadStrMiddleware(MiddlewareMixin):
+    def process_response ( self, request, response ):
+        hh = '<div style="background-color:#9df;border:2px solid #00f;'\
+                           'padding-right: 30px; border-radius:5px;">'\
+               '<div style="text-align:left">'\
+                 '<h1 ><a href="/">Back to HOME </a></h1>'\
+               '</div>'\
+               '<div style="text-align:right">'\
+                 '<h3><a href="/students/logout/">Log out</a></h3>'\
+               '</div>'\
+             '</div>'
+
+        if 'admin' not  in request.path:
+            response.content = '%s%s' % (hh, response.content)
+        return response
+
 
 
